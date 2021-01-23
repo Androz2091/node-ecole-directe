d802bec33a02a240a7f6aac107e5608ddd17d1f4
Androz2091 <androz2091@gmail.com>
2021-01-23 09:45:47 +0100

    :sparkles: Add tests

---
+++ test.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const EcoleDirecte = require("./");
+const session = new EcoleDirecte.Session();
+(async () => {
+    const account = await session.connexion(process.env.ID_ELEVE, process.env.MDP_ELEVE);
+
+    const notes = await account.fetchNotes()
+
+    const notesPremierSemestre = notes.periodes.find((periode) => periode.periode === '1er Semestre').ensembleMatieres;
+    
+    const moyenneGenerale = notesPremierSemestre.moyenneGenerale;
+    const moyenneClasse = notesPremierSemestre.moyenneClasse;
+    const rang = notesPremierSemestre.rang;
+
+    console.log(moyenneGenerale);
+    console.log(moyenneClasse);
+    console.log(rang);
+
+    const obtenirMoyenneMatiere = (nomMatiere) => notesPremierSemestre.disciplines.find((dis) => dis.discipline === nomMatiere).moyenne;
+
+    console.log(obtenirMoyenneMatiere('PHYSIQ.CHIMIE&MATHS'))
+    console.log(obtenirMoyenneMatiere('HISTOIRE-GEOGRAPHIE'))
+    console.log(obtenirMoyenneMatiere('ESPAGNOL LV2'))
+
+})()
