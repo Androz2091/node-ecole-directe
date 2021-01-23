b1795600fe6c0350715dd9e37a53746d9997707c
Androz2091 <androz2091@gmail.com>
2021-01-23 09:46:24 +0100

    :art: Add missing semi

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const session = new EcoleDirecte.Session();
 (async () => {
     const account = await session.connexion(process.env.ID_ELEVE, process.env.MDP_ELEVE);
 
-    const notes = await account.fetchNotes()
+    const notes = await account.fetchNotes();
 
     const notesPremierSemestre = notes.periodes.find((periode) => periode.periode === '1er Semestre').ensembleMatieres;
     
@@ -20,8 +20,8 @@ const session = new EcoleDirecte.Session();
 
     const obtenirMoyenneMatiere = (nomMatiere) => notesPremierSemestre.disciplines.find((dis) => dis.discipline === nomMatiere).moyenne;
 
-    console.log(obtenirMoyenneMatiere('PHYSIQ.CHIMIE&MATHS'))
-    console.log(obtenirMoyenneMatiere('HISTOIRE-GEOGRAPHIE'))
-    console.log(obtenirMoyenneMatiere('ESPAGNOL LV2'))
+    console.log(obtenirMoyenneMatiere('PHYSIQ.CHIMIE&MATHS'));
+    console.log(obtenirMoyenneMatiere('HISTOIRE-GEOGRAPHIE'));
+    console.log(obtenirMoyenneMatiere('ESPAGNOL LV2'));
 
 })()
