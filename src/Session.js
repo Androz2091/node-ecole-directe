efd5268fc2ed40101024f369789f4c99d5281ee4
Volnetiks <volnetiks@gmail.com>
2022-01-08 10:15:34 +0100

    Started reworking the api

--- src/Session.js
+++ src/Session.js
@@ -1,65 +1,90 @@
 const fetch = require("node-fetch");
+const request = require("request");
 const Eleve = require("./Eleve");
 const Famille = require("./Famille");
 
 module.exports = class Session {
-    constructor() {}
+  constructor() {}
 
-    connexion(identifiant, motdepasse) {
-        return new Promise(async (resolve, reject) => {
-            const res = await fetch(
-                "https://api.ecoledirecte.com/v3/login.awp",
-                {
-                    method: "POST",
-                    body:
-                        "data=" +
-                        encodeURI(
-                            JSON.stringify({
-                                identifiant,
-                                motdepasse
-                            })
-                        )
-                }
-            );
-            const data = await res.json();
-            if (!data.token) return reject("Invalid credentials");
-            const compte = data.data.accounts[0];
-            this.typeCompte =
-                (compte.typeCompte === "1" || compte.typeCompte === '2')
-                    ? "Famille"
-                    : compte.typeCompte === "E"
-                    ? "Élève"
-                    : null;
-            switch (this.typeCompte) {
-                case 'Famille':
-                    const famille = new Famille(this, data.data);
-                    await famille.fetch(data.token);
-                    resolve(famille);
-                    break;
-                case 'Élève':
-                    const eleve = new Eleve(this, compte);
-                    this.token = data.token;
-                    resolve(eleve);
-                    break;
-                default:
-                    reject("This type of account isn't supported");
-                    break;
-            }
-        });
-    }
+  async connexion(identifiant, motdepasse) {
+    await request(
+      {
+        method: "POST",
+        url: "https://api.ecoledirecte.com/v3/login.awp",
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (X11; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0",
+        },
+        body:
+          "data=" +
+          encodeURI(
+            JSON.stringify({
+              identifiant,
+              motdepasse,
+            })
+          ),
+      },
+      (err, httpResponse, body) => {
+        if (err) {
+          return console.error("error:", err);
+        }
+      }
+    );
+    // new Promise(async (resolve, reject) => {
+    //     XMLHttpRequest
+    //   console.log("test");
+    //   fetch("https://api.ecoledirecte.com/v3/login.awp", {
+    //     method: "POST",
+    //     body:
+    //       "data=" +
+    //       encodeURI(
+    //         JSON.stringify({
+    //           identifiant,
+    //           motdepasse,
+    //         })
+    //       ),
+    //   }).then(function(data) {
+    //     console.log(data);
+    //     // const data = await dataJSON.json();
+    //     if (!data.token) return reject("Invalid Credentials.");
+    //     const compte = data.data.accounts[0];
+    //     this.typeCompte =
+    //       compte.typeCompte === "1" || compte.typeCompte === "2"
+    //         ? "Famille"
+    //         : compte.typeCompte === "E"
+    //         ? "Élève"
+    //         : null;
+    //     switch (this.typeCompte) {
+    //       case "Famille":
+    //         const famille = new Famille(this, data.data);
+    //         famille.fetch(data.token);
+    //         resolve(famille);
+    //         break;
+    //       case "Élève":
+    //         const eleve = new Eleve(this, compte);
+    //         this.token = data.token;
+    //         resolve(eleve);
+    //         break;
+    //       default:
+    //         reject("This type of account isn't supported");
+    //         break;
+    //     }
+    //   });
+    // });
+  }
 
-    async request(url, token, payload = {}) {
-        return new Promise(async resolve => {
-            const finalPayload = {
-                ...payload,
-                ...{ token: token || this.token }
-            };
-            const res = await fetch(url, {
-                method: "POST",
-                body: "data=" + encodeURI(JSON.stringify(finalPayload))
-            });
-            const data = await res.json();
-            resolve(data);
-        });
-    }
+  async request(url, token, payload = {}) {
+    return new Promise(async (resolve) => {
+      const finalPayload = {
+        ...payload,
+        ...{ token: token || this.token },
+      };
+      const res = await fetch(url, {
+        method: "POST",
+        body: "data=" + encodeURI(JSON.stringify(finalPayload)),
+      });
+      const data = await res.json();
+      resolve(data);
+    });
+  }
 };
--- test.js
+++ test.js
@@ -1,27 +1,40 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const EcoleDirecte = require("./");
 const session = new EcoleDirecte.Session();
-(async () => {
-    const account = await session.connexion(process.env.ID_ELEVE, process.env.MDP_ELEVE);
 
-    const notes = await account.fetchNotes();
+console.log("test");
 
-    const notesPremierSemestre = notes.periodes.find((periode) => periode.periode === '1er Semestre').ensembleMatieres;
-    
-    const moyenneGenerale = notesPremierSemestre.moyenneGenerale;
-    const moyenneClasse = notesPremierSemestre.moyenneClasse;
-    const rang = notesPremierSemestre.rang;
+testAllFunctions();
 
-    console.log(moyenneGenerale);
-    console.log(moyenneClasse);
-    console.log(rang);
+async function testAllFunctions() {
+  const account = await session.connexion(
+    process.env.ID_ELEVE,
+    process.env.MDP_ELEVE
+  );
 
-    const obtenirMoyenneMatiere = (nomMatiere) => notesPremierSemestre.disciplines.find((dis) => dis.discipline === nomMatiere).moyenne;
+  console.log(account);
 
-    console.log(obtenirMoyenneMatiere('PHYSIQ.CHIMIE&MATHS'));
-    console.log(obtenirMoyenneMatiere('HISTOIRE-GEOGRAPHIE'));
-    console.log(obtenirMoyenneMatiere('ESPAGNOL LV2'));
+  console.log(
+    "etsedjfnqesjiobqehifvqeoluhfbqseojhfbaQohjfbApubohaegbpfihaqebfpv"
+  );
+  //   const notes = await account.fetchNotes();
 
-})()
+  //   const notesPremierSemestre = notes.periodes.find(
+  //     (periode) => periode.periode === "1er Semestre"
+  //   ).ensembleMatieres;
+
+  //   const moyenneGenerale = notesPremierSemestre.moyenneGenerale;
+  //   const moyenneClasse = notesPremierSemestre.moyenneClasse;
+  //   const rang = notesPremierSemestre.rang;
+
+  //   console.log(moyenneGenerale);
+  //   console.log(moyenneClasse);
+  //   console.log(rang);
+
+  //   const obtenirMoyenneMatiere = (nomMatiere) =>
+  //     notesPremierSemestre.disciplines.find(
+  //       (dis) => dis.discipline === nomMatiere
+  //     ).moyenne;
+}
